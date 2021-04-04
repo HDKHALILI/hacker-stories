0731fd2399b479bacfbff077e5ad28981695eece
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2021-04-05 09:23:05 +1000

    build basic components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function useSemiPersistentState(key) {
+  const [value, setValue] = React.useState(localStorage.getItem(key) || "");
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+}
 
 function App() {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org",
+      author: "Jordan Walke",
+      num_comments: 5,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const [searchTerm, setSearchTerm] = useSemiPersistentState("search");
+
+  function handleSearch(event) {
+    setSearchTerm(event.target.value);
+  }
+
+  const searchedStories = stories.filter(story =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Hacker Stories</h1>
+      <InputWithLabel
+        id="search"
+        label="Search"
+        value={searchTerm}
+        onInputChange={handleSearch}
+      />
+      <hr />
+
+      <List list={searchedStories} />
+
+      <hr />
+    </div>
+  );
+}
+
+function List(props) {
+  return props.list.map(({ objectID, ...item }) => (
+    <Item key={objectID} {...item} />
+  ));
+}
+
+function Item({ url, title, author, num_comments, points }) {
+  return (
+    <div>
+      <span>
+        <a href={url}>{title}</a>
+      </span>
+      <span>{author}</span>
+      <span>{num_comments}</span>
+      <span>{points}</span>
+    </div>
+  );
+}
+
+function InputWithLabel({ id, label, value, type = "text", onInputChange }) {
+  return (
+    <div>
+      <label htmlFor={id}>{label}: </label>
+      <input type={type} value={value} id={id} onChange={onInputChange} />
+      <p>
+        Searching for <strong>{value}</strong>.
+      </p>
     </div>
   );
 }
