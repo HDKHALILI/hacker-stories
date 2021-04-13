001fbef112d6e03cc36d2f221a497dbce4c9e80b
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2021-04-13 12:57:34 +1000

    replace fetch with axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 function useSemiPersistentState(key) {
   const [value, setValue] = React.useState(localStorage.getItem(key) || "");
@@ -47,12 +48,12 @@ function App() {
   const handleFetchStories = React.useCallback(() => {
     if (!searchTerm) return;
     dispatchStories({ type: "STORIES_FETCH_INIT" });
-    fetch(url)
-      .then(response => response.json())
+    axios
+      .get(url)
       .then(result => {
         dispatchStories({
           type: "STORIES_FETCH_SUCCESS",
-          payload: result.hits,
+          payload: result.data.hits,
         });
       })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
