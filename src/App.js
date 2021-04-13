f0d72ae543c03de7ab26dd3b1652cf578b242525
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2021-04-13 13:02:43 +1000

    refact aynschronous code to use async/await

--- src/App.js
+++ src/App.js
@@ -45,18 +45,18 @@ function App() {
     setUrl(`${API_ENDPOINT}${searchTerm}`);
   }
 
-  const handleFetchStories = React.useCallback(() => {
-    if (!searchTerm) return;
+  const handleFetchStories = React.useCallback(async () => {
     dispatchStories({ type: "STORIES_FETCH_INIT" });
-    axios
-      .get(url)
-      .then(result => {
-        dispatchStories({
-          type: "STORIES_FETCH_SUCCESS",
-          payload: result.data.hits,
-        });
-      })
-      .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
+
+    try {
+      const result = await axios.get(url);
+      dispatchStories({
+        type: "STORIES_FETCH_SUCCESS",
+        payload: result.data.hits,
+      });
+    } catch {
+      dispatchStories({ type: "STORIES_FETCH_FAILURE" });
+    }
   }, [url]);
 
   function handleSearchInput(event) {
