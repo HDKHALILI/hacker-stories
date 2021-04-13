2dd8015f635fb5e9ae5212247d26e824d961c87c
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2021-04-13 17:03:25 +1000

    use css-in-css approach to styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 
-import "./App.css";
+import styles from "./App.module.css";
 
 function useSemiPersistentState(key) {
   const [value, setValue] = React.useState(localStorage.getItem(key) || "");
@@ -92,8 +92,8 @@ function App() {
   }, [handleFetchStories]);
 
   return (
-    <div className="container">
-      <h1 className="headline-primary">My Hacker Stories</h1>
+    <div className={styles.container}>
+      <h1 className={styles.headlinePrimary}>My Hacker Stories</h1>
       <SearchForm
         searchTerm={searchTerm}
         onSearchInput={handleSearchInput}
@@ -125,7 +125,7 @@ function Item({ item, onRemoveItem }) {
     onRemoveItem(item);
   }
   return (
-    <div className="item">
+    <div className={styles.item}>
       <span style={{ width: "40%" }}>
         <a href={item.url}>{item.title}</a>
       </span>
@@ -136,7 +136,7 @@ function Item({ item, onRemoveItem }) {
         <button
           type="button"
           onClick={handleRemoveItem}
-          className="button button_small"
+          className={`${styles.button} ${styles.buttonSmall}`}
         >
           Dismiss
         </button>
@@ -166,7 +166,7 @@ function InputWithLabel({
 
   return (
     <div>
-      <label htmlFor={id} className="label">
+      <label htmlFor={id} className={styles.label}>
         {children}
       </label>
       {/* B */}
@@ -176,7 +176,7 @@ function InputWithLabel({
         value={value}
         id={id}
         autoFocus={isFocused}
-        className="input"
+        className={styles.input}
         onChange={onInputChange}
       />
       <p>
@@ -188,7 +188,7 @@ function InputWithLabel({
 
 function SearchForm({ searchTerm, onSearchInput, onSearchSubmit }) {
   return (
-    <form onSubmit={onSearchSubmit} className="search-form">
+    <form onSubmit={onSearchSubmit} className={styles.searchForm}>
       <InputWithLabel
         id="search"
         value={searchTerm}
@@ -201,7 +201,7 @@ function SearchForm({ searchTerm, onSearchInput, onSearchSubmit }) {
       <button
         type="submit"
         disabled={!searchTerm}
-        className="button button_large"
+        className={`${styles.button} ${styles.buttonLarge}`}
       >
         Submit
       </button>
